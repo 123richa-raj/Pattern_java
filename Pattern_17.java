3c6c3fe9c12b4df31c1e831bbc3bad8840fc1c51
Richa Raj <110665175+123richa-raj@users.noreply.github.com>
2024-01-03 17:21:16 +0530

    Add files via upload

---
+++ Pattern_17.java
@@ -0,0 +1,44 @@
+package Pattern;
+import java.util.*;
+
+public class Pattern_17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				Scanner sc = new Scanner(System.in);
+				int n = sc.nextInt();
+				int row = 1;
+				int star = n / 2 ;
+				int space = n/3;
+				while (row <= n) {
+					// star
+					int i = 1;
+					while (i <= star) {
+						System.out.print("* ");
+						i++;
+					}
+					// space
+					int j = 1;
+					while (j <= space) {
+						System.out.print("  ");
+						j++;
+					}
+					// star
+					int k = 1;
+					while (k <= star) {
+						System.out.print("* ");
+						k++;
+					}
+					// next Row Prep
+					if (row <= n / 2) {
+						star--;
+						space += 2;
+					} else {
+						star++;
+						space -= 2;
+					}
+					row++;
+					System.out.println();
+				}
+	}
+}
