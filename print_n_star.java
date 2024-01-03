709337c256e0368842d0fd07608629b2af1c0b66
Richa Raj <110665175+123richa-raj@users.noreply.github.com>
2024-01-03 17:13:13 +0530

    Add files via upload

---
+++ print_n_star.java
@@ -0,0 +1,17 @@
+package Pattern;
+import java.util.*;
+
+public class print_n_star {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i = 1;
+		while (i <= n) {
+			System.out.print("* ");
+			i++;
+		}
+
+	}
+}
