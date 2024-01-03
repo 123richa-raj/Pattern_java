a5a6cd12533072ebd1e22fda49b348a5d94a1e2b
Richa Raj <110665175+123richa-raj@users.noreply.github.com>
2024-01-03 17:15:22 +0530

    Add files via upload

---
+++ Pattern_5.java
@@ -0,0 +1,34 @@
+package Pattern;
+import java.util.*;
+
+public class Pattern_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+						Scanner sc = new Scanner(System.in);
+						int n = sc.nextInt();
+						int row = 1;
+						int star = n;
+						int space = 0;
+						while (row <= n) {
+							// space
+							int i = 1;
+							while (i <= space) {
+								System.out.print("  ");
+								i++;
+							}
+							// star
+							int j = 1;
+							while (j <= star) {
+								System.out.print("* ");
+								j++;
+							}
+
+							// next row Prep
+							row++;
+							System.out.println();
+							space++;
+							star--;
+						}
+	}
+}
