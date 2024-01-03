4a9506a8e331024bd72b44fe72daf1d787ed7487
Richa Raj <110665175+123richa-raj@users.noreply.github.com>
2024-01-03 17:19:13 +0530

    Add files via upload

---
+++ Pattern_24.java
@@ -0,0 +1,33 @@
+package Pattern;
+import java.util.*;
+
+public class Pattern_24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+						Scanner sc = new Scanner(System.in);
+						int n = sc.nextInt();
+						int row = 1;
+						int star = 1;
+						int space = n - 1;
+						while (row <= n) {
+							// space
+							int i = 1;
+							while (i <= space) {
+								System.out.print("\t");//tab 
+								i++;
+							}
+							// star
+							int j = 1;
+							while (j <= star) {
+								System.out.print(row + "\t");
+								j++;
+							}
+							// next Row Prep
+							System.out.println();
+							row++;
+							star += 2;
+							space--;
+						}
+	}
+}
